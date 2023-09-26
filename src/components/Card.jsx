17383a13116630070a4aa4435cf1024a080a5053
Alejo Bohórquez <therosseanux@gmail.com>
2023-09-26 18:51:19 -0500

    cards section ended

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({img, title, text}) => {
   return (
-    <div className='card'>Card</div>
+    <div className='card'>
+        <div>
+            <img className='card-image' src={img} alt="ux ui image" />
+        </div>
+        <div className='card-texts'>
+            <h2 className="card-title">{title}</h2>
+            <p className="text">{text}</p>
+            <a className='card-link' href="#">Más...</a>
+        </div>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,13 +49,42 @@ const Home = () => {
             <h2 className='big-title'>Nuestras rutas</h2>
             <p className='text m-t'>Descubre el mundo del desarrollo y preparate en:</p>
             <div className="container full-width">
-                <Card className='card'>Hola</Card>
-                <p className='card'>hola</p>
-                <p className='card'>hola</p>
-                <p className='card'>hola</p>
-                <p className='card'>hola</p>
-                <p className='card'>hola</p>
+                <Card 
+                    img='assets\image30.png' 
+                    title='UX/UI' 
+                    text='La ruta de UI/UX te llevará a través de diferentes niveles y temas clave para convertirte en un profesional en diseño de productos digit...'
+                />
+                <Card 
+                    img='assets\Rectangle30.png' 
+                    title='Project Manager' 
+                    text='La ruta de Project Manager te llevará a través de diferentes niveles y temas clave para desarrollar habilidades en...'
+                />
+                <Card 
+                    img='assets\Rectangle30-1.png' 
+                    title='QA' 
+                    text='La ruta de QA te proporcionará los conocimientos esenciales para convertirte en un profesional en...'
+                />
+                <Card 
+                    img='assets\Rectangle30-2.png' 
+                    title='FullStack Developer' 
+                    text='A lo largo de esta ruta, desarrollarás las habilidades y conocimientos necesarios para convertirte en un desarrollador versátil y competente...'
+                />
+                <Card 
+                    img='assets\Rectangle 30-3.png' 
+                    title='Product Manager' 
+                    text='La ruta de Product Manager te guiará a través de diferentes niveles y temas clave para convertirte en un profesional en gestión...'
+                />
+                <Card 
+                    img='assets\Rectangle 30-1.png' 
+                    title='Liderazgo' 
+                    text='Descubre una ruta de aprendizaje diseñada para potenciar tu liderazgo y guiarte hacia el éxito profesional. En esta...'
+                />
             </div>
+        </div>
+
+        <div className="container direction">
+            <h2 className='big-title'>¿Qué dicen nuestros estudiantes?</h2>
+            <div className="slider"></div>
         </div>
     </>
   )
